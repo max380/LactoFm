7da5f7a73718e6646954eeee5325337c3eac2977
Maxim <mr.max380@ya.ru>
2019-02-09 14:57:30 +0300

    ver 1.7

---
+++ js/index.js
@@ -0,0 +1,16 @@
+// Add to index.js or the first page that loads with your app.
+// For Intel XDK and please add this to your app.js.
+
+document.addEventListener('deviceready', function () {
+  // Enable to debug issues.
+  // window.plugins.OneSignal.setLogLevel({logLevel: 4, visualLevel: 4});
+  
+  var notificationOpenedCallback = function(jsonData) {
+    console.log('notificationOpenedCallback: ' + JSON.stringify(jsonData));
+  };
+
+  window.plugins.OneSignal
+    .startInit("d23fbfb6-48da-4aa0-9ff1-492844d4e324")
+    .handleNotificationOpened(notificationOpenedCallback)
+    .endInit();
+}, false);
