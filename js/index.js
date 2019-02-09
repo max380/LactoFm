97d1e5514c2b427aaef0ebd138fbb577dded7fa2
Maxim <mr.max380@ya.ru>
2019-02-09 15:47:55 +0300

    ver 1.7

--- js/index.js
+++ js/index.js
@@ -3,7 +3,7 @@
 
 document.addEventListener('deviceready', function () {
   // Enable to debug issues.
-  // window.plugins.OneSignal.setLogLevel({logLevel: 4, visualLevel: 4});
+  window.plugins.OneSignal.setLogLevel({logLevel: 4, visualLevel: 4});
   
   var notificationOpenedCallback = function(jsonData) {
     console.log('notificationOpenedCallback: ' + JSON.stringify(jsonData));
