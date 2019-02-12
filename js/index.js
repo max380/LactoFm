7d17df31beab7f53cf5b83eb7bf0ddb645ef5b5e
Maxim <mr.max380@ya.ru>
2019-02-12 10:07:26 +0300

    ver 1.8

--- js/index.js
+++ js/index.js
@@ -3,14 +3,14 @@
 
 document.addEventListener('deviceready', function () {
   // Enable to debug issues.
-  window.plugins.OneSignal.setLogLevel({logLevel: 4, visualLevel: 4});
+  // window.plugins.OneSignal.setLogLevel({logLevel: 4, visualLevel: 4});
   
   var notificationOpenedCallback = function(jsonData) {
     console.log('notificationOpenedCallback: ' + JSON.stringify(jsonData));
   };
 
   window.plugins.OneSignal
-    .startInit("d23fbfb6-48da-4aa0-9ff1-492844d4e324")
+    .startInit("e80c05ae-a7ee-4c34-947c-02b57e3e64c0")
     .handleNotificationOpened(notificationOpenedCallback)
     .endInit();
 }, false);
