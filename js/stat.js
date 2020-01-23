959cf5afc8a195437919f34b6cfeb5181e7722d6
Maxim <mr.max380@ya.ru>
2020-01-24 00:40:18 +0300

    ver 3.3

--- js/stat.js
+++ js/stat.js
@@ -1,6 +1,6 @@
 
      $( document ).ready(function() {
-         function updateTrack(){
+         function updateauthor(){
              $.ajax({
                  // Дополнительно передается два параметра
                  // callback & format для получения данных
@@ -10,36 +10,58 @@
                  dataType: "jsonp",
                  method: "GET",
                  success: function( response ) {
-                     var trackTitlte = response.objects[0].metadata;
-					 $("#track").text(trackTitlte);
-					 var trackTitlte1 = response.objects[1].metadata;
-					 $("#track1").text(trackTitlte1);
-					 var trackTitlte2 = response.objects[2].metadata;
-					 $("#track2").text(trackTitlte2);
-					 var trackTitlte3 = response.objects[3].metadata;
-					 $("#track3").text(trackTitlte3);
-					 var trackTitlte4 = response.objects[4].metadata;
-					 $("#track4").text(trackTitlte4);
-					 var trackTitlte5 = response.objects[5].metadata;
-					 $("#track5").text(trackTitlte5);
-					 var trackTitlte6 = response.objects[6].metadata;
-					 $("#track6").text(trackTitlte6);
-					 var trackTitlte7 = response.objects[7].metadata;
-					 $("#track7").text(trackTitlte7);
-					 var trackTitlte8 = response.objects[8].metadata;
-					 $("#track8").text(trackTitlte8);
-					 var trackTitlte9 = response.objects[9].metadata;
-					 $("#track9").text(trackTitlte9);
-					 var trackTitlte10 = response.objects[10].metadata;
-					 $("#track10").text(trackTitlte10);
-					
+                     var authorTitlte = response.objects[0].author;
+					 $("#author").text(authorTitlte);
+					 var authorTitlte1 = response.objects[1].author;
+					 $("#author1").text(authorTitlte1);
+					 var authorTitlte2 = response.objects[2].author;
+					 $("#author2").text(authorTitlte2);
+					 var authorTitlte3 = response.objects[3].author;
+					 $("#author3").text(authorTitlte3);
+					 var authorTitlte4 = response.objects[4].author;
+					 $("#author4").text(authorTitlte4);
+					 var authorTitlte5 = response.objects[5].author;
+					 $("#author5").text(authorTitlte5);
+					 var authorTitlte6 = response.objects[6].author;
+					 $("#author6").text(authorTitlte6);
+					 var authorTitlte7 = response.objects[7].author;
+					 $("#author7").text(authorTitlte7);
+					 var authorTitlte8 = response.objects[8].author;
+					 $("#author8").text(authorTitlte8);
+					 var authorTitlte9 = response.objects[9].author;
+					 $("#author9").text(authorTitlte9);
+					 var authorTitlte10 = response.objects[10].author;
+					 $("#author10").text(authorTitlte10);
+                     var authorTitlte = response.objects[0].title;
+					 $("#title").text(authorTitlte);
+					 var authorTitlte1 = response.objects[1].author;
+					 $("#title1").text(authorTitlte1);
+					 var authorTitlte2 = response.objects[2].title;
+					 $("#title2").text(authorTitlte2);
+					 var authorTitlte3 = response.objects[3].title;
+					 $("#title3").text(authorTitlte3);
+					 var authorTitlte4 = response.objects[4].title;
+					 $("#title4").text(authorTitlte4);
+					 var authorTitlte5 = response.objects[5].title;
+					 $("#title5").text(authorTitlte5);
+					 var authorTitlte6 = response.objects[6].title;
+					 $("#title6").text(authorTitlte6);
+					 var authorTitlte7 = response.objects[7].title;
+					 $("#title7").text(authorTitlte7);
+					 var authorTitlte8 = response.objects[8].title;
+					 $("#title8").text(authorTitlte8);
+					 var authorTitlte9 = response.objects[9].title;
+					 $("#title9").text(authorTitlte9);
+					 var authorTitlte10 = response.objects[10].title;
+					 $("#title10").text(authorTitlte10);					
 					 
                  }
              });
          }
-         updateTrack();
-         setInterval(updateTrack, 5*1000);
+         updateauthor();
+         setInterval(updateauthor, 5*1000);
      });
+
 
 	 
      $( document ).ready(function() {
