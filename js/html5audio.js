71f887c043867d62ab72e7cfccd869b97682ec12
Maxim <mr.max380@ya.ru>
2020-02-08 02:35:15 +0300

    ver 3.5

--- js/html5audio.js
+++ js/html5audio.js
@@ -1,5 +1,4 @@
 
-
 var progressTimer;
 
 var playButton;
@@ -21,30 +20,42 @@ function onConfirmRetry(button) {
 function pad2(number) {
 	return (number < 10 ? '0' : '') + number
 }
+	
 
-var myaudioURL = 'http://89.163.144.28:8000/stream';
-var myaudio = new Audio(myaudioURL);
-var isPlaying = false;
 var readyStateInterval = null;
 
 var html5audio = {
 	play: function()
 	{
 		isPlaying = true;
+		myaudioURL = 'http://play.lactosefm.ru:8110/stream';
+		myaudio = new Audio(myaudioURL);
 		myaudio.play();
+		
+	
 	
 		readyStateInterval = setInterval(function(){
-			 if (myaudio.readyState <= 2) {
+			 if (myaudio.readyState <= 1) {
 				 playButton.style.display = 'none';
 				 activityIndicator.style.display = 'block';
 				 textPosition.innerHTML = 'loading...';
+				 test2();
 			 }
 		},10000);
+		myaudio.addEventListener("timeupdate", function() {
+			 var s = parseInt(myaudio.currentTime % 60);
+			 var m = parseInt((myaudio.currentTime / 60) % 60);
+			 var h = parseInt(((myaudio.currentTime / 60) / 60) % 60);
+			 if (isPlaying && myaudio.currentTime > 0) {
+				 textPosition.innerHTML = pad2(h) + ':' + pad2(m) + ':' + pad2(s);
+			 }
+		}, false);
 		myaudio.addEventListener("error", function() {
-			 // console.log('myaudio ERROR');
+			 console.log('myaudio ERROR');
+			 test2();
 		}, false);
 		myaudio.addEventListener("canplay", function() {
-			 // console.log('myaudio CAN PLAY');
+			 console.log('myaudio CAN PLAY');
 		}, false);
 		myaudio.addEventListener("waiting", function() {
 			 //console.log('myaudio WAITING');
@@ -52,24 +63,27 @@ var html5audio = {
 			 playButton.style.display = 'none';
 			 stopButton.style.display = 'none';
 			 activityIndicator.style.display = 'block';
+			test2();
 		}, false);
 		myaudio.addEventListener("playing", function() {
 			 isPlaying = true;
 			 playButton.style.display = 'none';
 			 activityIndicator.style.display = 'none';
 			 stopButton.style.display = 'block';
+			 test1();	
 		}, false);
 		myaudio.addEventListener("ended", function() {
-			 //console.log('myaudio ENDED');
-			 html5audio.stop();
-			 // navigator.notification.alert('Streaming failed. Possibly due to a network error.', null, 'Stream error', 'OK');
-			 // navigator.notification.confirm(
-			 //	'Streaming failed. Possibly due to a network error.', // message
-			 //	onConfirmRetry,	// callback to invoke with index of button pressed
-			 //	'Stream error',	// title
-			 //	'Retry,OK'		// buttonLabels
-			 // );
-			 if (window.confirm('Ваш телефон потерял соединение. Давайте попробуем снова!')) {
+			 console.log('myaudio ENDED');
+			html5audio.stop();
+			test2();	
+			 navigator.notification.alert('Streaming failed. Possibly due to a network error.', null, 'Stream error', 'OK');
+			  navigator.notification.confirm(
+			 	'Streaming failed. Possibly due to a network error.', // message
+			 	onConfirmRetry,	// callback to invoke with index of button pressed
+			 	'Stream error',	// title
+			 	'Retry,OK'		// buttonLabels
+			  );
+			 if (window.confirm('Streaming failed. Possibly due to a network error. Retry?')) {
 			 	onConfirmRetry();
 			 }
 		}, false);
@@ -78,19 +92,21 @@ var html5audio = {
 		isPlaying = false;
 		clearInterval(readyStateInterval);
 		myaudio.pause();
-		stopButton.style.display = 'none';
-		activityIndicator.style.display = 'none';
-		playButton.style.display = 'block';
-	},
-	stop: function() {
-		isPlaying = false;
-		clearInterval(readyStateInterval);
-		myaudio.currentTime = "0"
+		test2();			
 		stopButton.style.display = 'none';
 		activityIndicator.style.display = 'none';
 		playButton.style.display = 'block';
 		myaudio = null;
-		myaudio = new Audio(myaudioURL);
+	},
+	stop: function() {
+		isPlaying = false;
+		clearInterval(readyStateInterval);
+		myaudio.pause();
+		test2();	
+		stopButton.style.display = 'none';
+		activityIndicator.style.display = 'none';
+		playButton.style.display = 'block';
+		myaudio = null;
 		textPosition.innerHTML = '';
 	}
 };
